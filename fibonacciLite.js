1e60766cd2389eb85bf20819b237da6d5835a1c7
Travis Clarke <travis.m.clarke@gmail.com>
2018-07-10 14:40:09 -0700

    add fibonacciLite solution

---
+++ fibonacciLite.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * Fibonacci Lite
+ *
+ * @param {number} n – nth fibonacci number
+ * @return {number}
+ */
+
+function fibonacciLite(n, memo = {}) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  if (memo[n]) return memo[n];
+
+  memo[n] = fibonacciLite(n - 1, memo) + fibonacciLite(n - 2, memo);
+  return memo[n];
+}
+
+// Test
+if (require.main === module) {
+  console.log(fibonacciLite(3)); // 2
+  console.log(fibonacciLite(12)); // 144
+  console.log(fibonacciLite(30)); // 832040
+}
+
+module.exports = {
+  fibonacciLite
+};
